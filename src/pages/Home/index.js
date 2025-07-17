e72c2fc63943053ae3de546f58c2d665908b4884
JoaoDev20 <joaovictorapso06@gmail.com>
2025-07-17 14:47:09 -0300

    Criando Header do projeto

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ function Home() {
   return (
     <div>
       <h1>Bem vindo a pagina home</h1>
-      <Link to="/filme/:id">Filmes em alta</Link>
+      <Link to="/filme/2456">Filmes em alta</Link>
     </div>
   );
 }
