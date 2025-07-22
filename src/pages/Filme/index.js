13183d55ffc9fffb388ebc3193c4ac070cfb2157
JoaoDev20 <joaovictorapso06@gmail.com>
2025-07-22 17:00:15 -0300

    Finalizado a parte de detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,5 +1,6 @@
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import "./filme-info.css";
 
 import api from "../../services/api";
 function Filme() {
@@ -50,7 +51,14 @@ function Filme() {
       <h3>Sinopse</h3>
       <span>{filme.overview}</span>
 
-      <strong>Avaliação: {filme.vote_average} /10</strong>
+      <strong>Avaliação: {filme.vote_average.toFixed(1)} /10</strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a href="#">Trailer</a>
+        </button>
+      </div>
     </div>
   );
 }
